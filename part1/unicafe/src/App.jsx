64d01979eb6bc47aeafa1cf207edf2fdb1db5d91
Artturi Siven <artturionni@gmail.com>
2026-04-11 15:47:50 +0300

    Solutions to 1.8: unicafe step 3 and 1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,16 +5,38 @@ const Display = (props) => (
     {props.rating} {props.value}
   </div>
 );
+const Statistics = ({ good, neutral, bad }) => {
+  const allRatings = good + neutral + bad;
+  const average = (good - bad) / allRatings || 0;
+  const positive = (good / allRatings) * 100 || 0;
+
+  if (allRatings === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <Display rating="good" value={good} />
+        <Display rating="neutral" value={neutral} />
+        <Display rating="bad" value={bad} />
+        <Display rating="all" value={allRatings} />
+        <Display rating="average" value={average} />
+        <Display rating="positive" value={positive + " %"} />
+      </div>
+    );
+  }
+};
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const allRatings = good + neutral + bad;
-  const average = (good - bad) / allRatings || 0;
-  const positive = (good / allRatings) * 100 || 0;
 
   const handleGood = () => {
     setGood(good + 1);
@@ -32,13 +54,7 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <h1>statistics</h1>
-      <Display rating="good" value={good} />
-      <Display rating="neutral" value={neutral} />
-      <Display rating="bad" value={bad} />
-      <Display rating="all" value={allRatings} />
-      <Display rating="average" value={average} />
-      <Display rating="positive" value={positive + " %"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
