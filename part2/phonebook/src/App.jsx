97aa93e0366d8b5ad4b4e0bbe2a9e5f3385d1b15
Artturi Siven <artturionni@gmail.com>
2026-04-22 20:22:29 +0300

    Solutions to 2.6. and 2.7. The Phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    };
+    if (persons.some((person) => person.name === newName)) {
+      const alertMessage = `${newName} is already added to the phonebook`;
+      alert(alertMessage);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
