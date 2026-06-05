d8c1871e6d8259b454f9d996da9b2a7ebe79b451
Artturi Siven <artturionni@gmail.com>
2026-06-05 23:36:15 +0300

    Solution to 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -25,4 +26,30 @@ test('renders content', () => {
 
   const likes = screen.getByText('likes 5')
   expect(likes).not.toBeVisible()
+})
+
+test('clicking view button shows url and likes', async () => {
+  const blog = {
+    title: 'This is a test',
+    author: 'Test author',
+    url: 'test.com',
+    likes: 5,
+    user: {
+      name: 'tester',
+      username: 'tester',
+      id: '1234'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const url = screen.getByText('test.com')
+  expect(url).toBeVisible()
+
+  const likes = screen.getByText('likes 5')
+  expect(likes).toBeVisible()
 })
