b33b41d473c3522c78e894afad6a708f307bfb0a
Artturi Siven <artturionni@gmail.com>
2026-06-22 21:46:57 +0300

    Solution to 5.18 & 5.19: Blog List End To End Testing, step 2 & 3

--- part5/blog-e2e/tests/blog_app.spec.js
+++ part5/blog-e2e/tests/blog_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        username: 'Artt',
+        name: 'Artturi Siven',
+        password: '1234'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,5 +20,41 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('Artt')
+      await page.getByLabel('password').fill('1234')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Artturi Siven logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('Artt')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'Artt', '1234')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+
+      await page.getByPlaceholder('enter blog title').fill('E2E test blog')
+      await page.getByPlaceholder('enter blog author').fill('Test Author')
+      await page.getByPlaceholder('enter blog url').fill('http://test.com')
+
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(page.locator('.blog-summary')).toContainText('E2E test blog Test Author')
+    })
   })
 })
