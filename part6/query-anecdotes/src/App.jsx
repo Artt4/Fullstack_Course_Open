9fe4fc74406a850989d21bc61e8972e16031ad41
Artturi Siven <artturionni@gmail.com>
2026-07-24 21:19:12 +0300

    part 6: completed exercises 6.16-6.19

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,40 @@
+import { useAnecdotes } from './hooks/useAnecdotes'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const { anecdotes, isPending, isError, voteAnecdote } = useAnecdotes()
+
+  const handleVote = (anecdote) => {
+    voteAnecdote(anecdote)
+    console.log('vote')
+  }
+
+  if (isPending) {
+    return <div>loading data...</div>
+  }
+  if (isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
